3c477fd50af0aba6dbfb899cd9c266332da2c9cf
Hokage <thotpbank050699@gmail.com>
2020-12-19 11:23:05 +0700

    re init

--- services/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use(require("./auth/router"));
-router.use(require("./courses/router"));
-router.use(require("./lessons/router"));
-
-module.exports = router;
